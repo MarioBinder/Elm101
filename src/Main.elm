207501c42aca67c34966b9303b78cda4378f6d10
mbinder <mario.binder@wolff-dmm.de>
2021-08-18 19:29:53 +0200

    hello gi

---
+++ src/Main.elm
@@ -0,0 +1,337 @@
+{--
+    https://elm-lang.org/docs/syntax
+    https://package.elm-lang.org/packages/elm/core/1.0.5/
+    https://package.elm-lang.org/packages/rundis/elm-bootstrap/latest/Bootstrap-Accordion
+    http://elm-bootstrap.info/getting-started
+    http://elm-bootstrap.info/accordion
+--}
+
+module Main exposing (main)
+
+import Bootstrap.Accordion as Accordion
+import Bootstrap.Button as Button
+import Bootstrap.CDN as CDN
+import Bootstrap.Card as Card
+import Bootstrap.Card.Block as Block
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.ListGroup as Listgroup
+import Bootstrap.Modal as Modal
+import Bootstrap.Navbar as Navbar
+import Bootstrap.Text as Text
+import Browser exposing (UrlRequest)
+import Browser.Navigation as Navigation
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Url exposing (Url)
+import Url.Parser as UrlParser exposing ((</>), Parser, top)
+
+type alias Flags =
+    {}
+
+
+type alias Model =
+    { navKey : Navigation.Key
+    , page : Page
+    , navState : Navbar.State
+    , modalVisibility : Modal.Visibility
+    , accordionState : Accordion.State
+    }
+
+
+type Page
+    = Home
+    | GettingStarted
+    | AccordionPage
+    | Modules
+    | NotFound
+
+
+main : Program Flags Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = ClickedLink
+        , onUrlChange = UrlChange
+        }
+
+
+init : Flags -> Url -> Navigation.Key -> ( Model, Cmd Msg )
+init _ url key =
+    let
+        ( navState, navCmd ) =
+            Navbar.initialState NavMsg
+
+        ( model, urlCmd ) =
+            urlUpdate url
+                { navKey = key
+                , navState = navState
+                , page = Home
+                , modalVisibility = Modal.hidden
+                , accordionState = Accordion.initialStateCardOpen "card1"
+                }
+    in
+    ( model, Cmd.batch [ urlCmd, navCmd ] )
+
+
+type Msg
+    = UrlChange Url
+    | ClickedLink UrlRequest
+    | NavMsg Navbar.State
+    | CloseModal
+    | ShowModal
+    | AccordionMsg Accordion.State
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ Navbar.subscriptions model.navState NavMsg
+        , Accordion.subscriptions model.accordionState AccordionMsg
+        ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ClickedLink req ->
+            case req of
+                Browser.Internal url ->
+                    ( model, Navigation.pushUrl model.navKey <| Url.toString url )
+
+                Browser.External href ->
+                    ( model, Navigation.load href )
+
+        UrlChange url ->
+            urlUpdate url model
+
+        NavMsg state ->
+            ( { model | navState = state }
+            , Cmd.none
+            )
+
+        CloseModal ->
+            ( { model | modalVisibility = Modal.hidden }
+            , Cmd.none
+            )
+
+        ShowModal ->
+            ( { model | modalVisibility = Modal.shown }
+            , Cmd.none
+            )
+
+        AccordionMsg state ->
+            ( { model | accordionState = state }
+            , Cmd.none
+            )
+
+
+urlUpdate : Url -> Model -> ( Model, Cmd Msg )
+urlUpdate url model =
+    case decode url of
+        Nothing ->
+            ( { model | page = NotFound }, Cmd.none )
+
+        Just route ->
+            ( { model | page = route }, Cmd.none )
+
+
+decode : Url -> Maybe Page
+decode url =
+    { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
+        |> UrlParser.parse routeParser
+
+
+routeParser : Parser (Page -> a) a
+routeParser =
+    UrlParser.oneOf
+        [ UrlParser.map Home top
+        , UrlParser.map GettingStarted (UrlParser.s "getting-started")
+        , UrlParser.map AccordionPage (UrlParser.s "accordionPage")
+        , UrlParser.map Modules (UrlParser.s "modules")
+        ]
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Elm Bootstrap 2"
+    , body =
+        [ CDN.stylesheet
+        , div []
+            [ menu model
+            , mainContent model
+            , modal model
+            ]
+        ]
+    }
+
+
+menu : Model -> Html Msg
+menu model =
+    Navbar.config NavMsg
+        |> Navbar.withAnimation
+        |> Navbar.container
+        |> Navbar.brand [ href "#" ] [ text "Elm Bootstrap 2.0" ]
+        |> Navbar.items
+            [ Navbar.itemLink [ href "#getting-started" ] [ text "Getting started" ]
+            , Navbar.itemLink [ href "#modules" ] [ text "Modules" ]
+            , Navbar.itemLink [ href "#accordionPage" ] [ text "Accordionpage" ]
+            ]
+        |> Navbar.view model.navState
+
+
+mainContent : Model -> Html Msg
+mainContent model =
+    Grid.container [] <|
+        case model.page of
+            Home ->
+                pageHome model
+
+            GettingStarted ->
+                pageGettingStarted model
+
+            AccordionPage ->
+                accordionPage model
+
+            Modules ->
+                pageModules model
+
+            NotFound ->
+                pageNotFound
+
+
+pageHome : Model -> List (Html Msg)
+pageHome _ =
+    [ h1 [] [ text "Home" ]
+    , Grid.row []
+        [ Grid.col []
+            [ Card.config [ Card.outlineSecondary ]
+                |> Card.headerH4 [] [ text "Getting started" ]
+                |> Card.footer [] [ text "Footer" ]
+                |> Card.block []
+                    [ Block.text [] [ text "Getting started is real easy. Just click the start button." ]
+                    , Block.custom <|
+                        Button.linkButton
+                            [ Button.primary, Button.attrs [ href "#getting-started" ] ]
+                            [ text "Start" ]
+                    ]
+                |> Card.view
+            ]
+        , Grid.col []
+            [ Card.config [ Card.outlineSecondary ]
+                |> Card.headerH4 [] [ text "Modules" ]
+                |> Card.footer [] [ text "Footer" ]
+                |> Card.block []
+                    [ Block.text [] [ text "Check out the modules overview" ]
+                    , Block.custom <|
+                        Button.linkButton
+                            [ Button.primary, Button.attrs [ href "#modules" ] ]
+                            [ text "Module" ]
+                    ]
+                |> Card.view
+            ]
+        ]
+    ]
+
+
+accordionPage : Model -> List (Html Msg)
+accordionPage model =
+    [ h2 [] [ text "AccordionPage" ]
+    , Accordion.config AccordionMsg
+        |> Accordion.withAnimation
+        |> Accordion.onlyOneOpen
+        |> Accordion.cards
+            [ Accordion.card
+                { id = "card1"
+                , options = [ Card.outlineSuccess, Card.align Text.alignXsCenter ]
+                , header =
+                    Accordion.headerH3 []
+                        (Accordion.toggle [] [ text " Card 1" ])
+                        |> Accordion.prependHeader
+                            [ span [ class "fa fa-car" ] [] ]
+                , blocks =
+                    [ Accordion.block [ Block.align Text.alignXlCenter ]
+                        [ Block.titleH4 [] [ text "Block title" ]
+                        , Block.text [] [ text "Lorem ipsum etc" ]
+                        ]
+                    , Accordion.block [ Block.align Text.alignXlCenter ]
+                        [ Block.titleH4 [] [ text "Block2 title" ]
+                        , Block.text [] [ text "Lorem ipsum etc" ]
+                        ]
+                    ]
+                }
+            , Accordion.card
+                { id = "card2"
+                , options = [ Card.outlineSuccess, Card.align Text.alignXsCenter ]
+                , header =
+                    Accordion.headerH3 []
+                        (Accordion.toggle [] [ text " Card 2" ])
+                        |> Accordion.prependHeader
+                            [ span [ class "fa fa-taxi" ] [] ]
+                , blocks =
+                    [ Accordion.block []
+                        [ Block.text [] [ text "Lorem ipsum etc" ] ]
+                    , Accordion.listGroup
+                        [ Listgroup.li [] [ text "List item 1" ]
+                        , Listgroup.li [] [ text "List item 2" ]
+                        ]
+                    ]
+                }
+            ]
+        |> Accordion.view model.accordionState
+    ]
+
+
+pageGettingStarted : Model -> List (Html Msg)
+pageGettingStarted _ =
+    [ h2 [] [ text "Getting started" ]
+    , Button.button
+        [ Button.success
+        , Button.large
+        , Button.block
+        , Button.attrs [ onClick ShowModal ]
+        ]
+        [ text "Click me" ]
+    ]
+
+
+pageModules : Model -> List (Html Msg)
+pageModules _ =
+    [ h1 [] [ text "Modules" ]
+    , Listgroup.ul
+        [ Listgroup.li [] [ text "Alert" ]
+        , Listgroup.li [] [ text "Badge" ]
+        , Listgroup.li [] [ text "Card" ]
+        ]
+    ]
+
+
+pageNotFound : List (Html Msg)
+pageNotFound =
+    [ h1 [] [ text "Not found" ]
+    , text "Sorry couldn't find that page"
+    ]
+
+
+modal : Model -> Html Msg
+modal model =
+    Modal.config CloseModal
+        |> Modal.small
+        |> Modal.h4 [] [ text "Getting started ?" ]
+        |> Modal.body []
+            [ Grid.containerFluid []
+                [ Grid.row []
+                    [ Grid.col
+                        [ Col.xs6 ]
+                        [ text "Col 1" ]
+                    , Grid.col
+                        [ Col.xs6 ]
+                        [ text "Col 2" ]
+                    ]
+                ]
+            ]
+        |> Modal.view model.modalVisibility
